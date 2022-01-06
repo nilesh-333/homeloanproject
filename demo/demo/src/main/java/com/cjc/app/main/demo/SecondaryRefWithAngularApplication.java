2e70400a360288833352a11fe7db94a0d1e84a37
nilesh-333 <gitte.nilesh3@gmail.com>
2022-01-06 10:24:47 +0530

    2nd commit

---
+++ demo/demo/src/main/java/com/cjc/app/main/demo/SecondaryRefWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.cjc.app.main.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondaryRefWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondaryRefWithAngularApplication.class, args);
+	}
+
+}
